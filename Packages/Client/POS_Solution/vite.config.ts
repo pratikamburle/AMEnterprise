81e9ce136ef043d5ba6ec01f442fd3a94f98d51f
Pratik Amburle <Pratik.Amburle@uk.fleetcor.com>
2025-12-20 17:31:56 +0530

    configure sub directory for publish

--- Packages/Client/POS_Solution/vite.config.ts
+++ Packages/Client/POS_Solution/vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/AMEnterprise/POS_Solution/",
   plugins: [react(), tailwindcss()],
 });
