da2176372d87bd9c70ac8ff118d06725e5be33e5
Pratik Amburle <Pratik.Amburle@uk.fleetcor.com>
2025-12-22 15:06:02 +0530

    add base name in browser router

--- Packages/client/pos-solution/src/App.tsx
+++ Packages/client/pos-solution/src/App.tsx
@@ -9,7 +9,7 @@ import { ReceiptView } from "./components/pos/ReceiptView";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/AMEnterprise/">
       <AppLayout>
         <Routes>
           <Route path="/" element={<Navigate to="/products" replace />} />
